1354b38274c074cfc777b91144c1bf8e40c5deac
Sled Oleksandr <sled.oleksandr@gmail.com>
2021-09-17 19:58:21 +0200

    fixed total and percent method

--- src/App.js
+++ src/App.js
@@ -6,17 +6,22 @@ import Statistics from './Components/Statistics/Statistics.js';
 import Notification from './Components/Notification/Notification.js';
 
 class App extends Component {
-  state = { good: 0, neutral: 0, bad: 0, total: 0 };
+  state = { good: 0, neutral: 0, bad: 0 };
 
   handelIncrement = ({ target: { name } }) => {
     this.setState({ [name]: this.state[name] + 1 });
-    this.setState((prevState) => {
-      return { total: prevState.good + prevState.neutral + prevState.bad };
-    });
   };
 
+  countTotalFeedback = ({ good, neutral, bad } = this.state) =>
+    good + neutral + bad;
+
+  countPositiveFeedbackPercentage = (
+    { good } = this.state,
+    total = this.countTotalFeedback()
+  ) => Math.round((good / total) * 100);
   render() {
-    const { good, neutral, bad, total } = this.state;
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
     const percent = Math.round((good / total) * 100);
     return (
       <div className="App">
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 class Statistics extends Component {
   render() {
-    let { good, neutral, bad } = this.props;
+    let { good, neutral, bad, total, percent } = this.props;
     return (
       <div>
         <h2>Statistics:</h2>
@@ -15,10 +15,10 @@ class Statistics extends Component {
         <p>
           {['bad']}:&ensp;{bad}
         </p>
-        <p>Total Feedback:&ensp;{this.props.total}</p>
+        <p>Total Feedback:&ensp;{total}</p>
         <p>
           Positive Feedback:&ensp;
-          {this.props.total > 0 ? this.props.percent : 0}%
+          {total > 0 ? percent : 0}%
         </p>
       </div>
     );
